b95aaab5bf13a9a5ebcb959611f00fa03afbeab9
hiromimatsu <hiro.ay12@icloud.com>
2020-07-28 09:38:19 +0900

    create_action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :move_to_index, expect: :index
+  
   def index
     @articles = Article.all
   end
@@ -7,4 +9,19 @@ class ArticlesController < ApplicationController
     @articles = Article.new
   end
 
+  def create
+    Article.create(article_params)
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:quote, :menu, :rule )
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
